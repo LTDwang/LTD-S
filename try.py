538381b5e5386729c07bfc7768561fc93e4564bb
LTDwang <1429168201@qq.com>
2021-09-29 23:14:11 +0800

    try2

--- try.py
+++ try.py
@@ -1,4 +1,10 @@
 from unabs import unabs
-print ("请键入数字")
-a =unabs(int(input()))
+from unabs import power
+print ("请键入3个数字")
+e =int(input())
+a =unabs(e)
+b =int(input())
+c =int(input())
+d =power(b, c)
 print (a)
+print (d)
