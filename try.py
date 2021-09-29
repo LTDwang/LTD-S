c5266a343d1c83f709c474cd3c26c58c1120fa91
LTDwang <1429168201@qq.com>
2021-09-29 23:37:21 +0800

    备注

--- try.py
+++ try.py
@@ -3,12 +3,14 @@ from unabs import power
 from unabs import imfor
 print ("请键入3个数字")
 e =int(input())
-a =unabs(e)
+a =unabs(e) ##unabs测试
+
 b =int(input())
 c =int(input())
-d =power(b, c)
+d =power(b, c) ##power测试
+
 f = input()
 g = input()
-imfor(f, g)
+imfor(f, g) ##imfor测试
 print (a)
 print (d)
