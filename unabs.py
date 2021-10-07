e0b20607a5c9fb5eb2b1d77d9beeb56b186a2f2c
LTDwang <1429168201@qq.com>
2021-10-08 00:11:39 +0800

    说明

--- unabs.py
+++ unabs.py
@@ -12,21 +12,21 @@ def power(x, n =2):
     while n > 1:
         n = n-1
         s = s * x
-    return s
+    return s ##次方工具
 
 def imfor(name, age):
      print ("name:", name)
-     print ("age:", age)
+     print ("age:", age) ##信息收集
 
 def get(f, g, h):
     b =(f, g, h)
     a =b[0]
-    print (a)
+    print (a) ##选取第一个元素
 
 def jiecheng(i):
     if i ==1:
         return 1
-    return i * jiecheng(i-1)
+    return i * jiecheng(i-1) ##阶乘好耶！
 
 
     
